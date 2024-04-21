4d2c98e3385dff0df9da6a1142e70473169c2d8d
vano tatulashvili <vanotimmy@gmail.com>
2024-04-21 17:14:30 +0400

    Added Little correction: "@FindBy" annotation style

--- src/test/java/org/epam/TestApp.java
+++ src/test/java/org/epam/TestApp.java
@@ -2,15 +2,27 @@ package org.epam;
 
 
 import org.epam.pages.LoginPage;
+import org.junit.Assert;
+import org.junit.FixMethodOrder;
 import org.junit.Test;
+import org.junit.runners.MethodSorters;
 
+@FixMethodOrder(MethodSorters.NAME_ASCENDING)
 public class TestApp extends BaseTest {
 
     LoginPage loginPage = new LoginPage ( driver );
 
     @Test
-    public void testLoginScreen () {
+    public void test_1_LoginScreen () {
         loginPage.clickLoginScreen ( );
+    }
+
+    @Test
+    public void test_2_LoginCorrectCredentials(){
+        String username = "alice";
+        String password = "mypassword";
+        String actualDisplayedName = loginPage.loginToApp ( username,password );
+        Assert.assertEquals("You are logged in as alice", actualDisplayedName);
     }
 
 }
